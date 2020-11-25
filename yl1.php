b00f4c185f182a698556a8f1b17af9ae1526693f
Rain Kohv <rain.kohv@khk.ee>
2020-11-25 18:23:59 +0200

    ülesanne 1

---
+++ yl1.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Ülesanne 1
+ * Rain Kohv
+ * 25.11.2020
+ */
+$nimi = "Rain";
+$synniaasta = "1994";
+$tahtkuju = "kaljukits";
+
+echo "Minu nimi on $nimi. \n Olen sündinud aastal $synniaasta. \n Minu tähtkuju on $tahtkuju. \n";
+
+echo "\"It’s My Life\" – Dr. Alban \n";
+
+echo "(\(\ \n";
+echo "( -.-) \n";
+echo 'o_(")(")';
+?>
