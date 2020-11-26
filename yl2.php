50fa2e7f8b82923ade78e23729568e37f1232bdb
Rain Kohv <rain.kohv@khk.ee>
2020-11-26 12:38:20 +0200

    ülesanne 2

---
+++ yl2.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Ülesanne 2
+ * Rain Kohv
+ * 26.11.2020
+ */
+
+$x = 3;
+$y = 5;
+
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+
+echo "$x + $y = $liitm<br>";
+echo "$x - $y = $lahut<br>";
+echo "$x * $y = $korru<br>";
+echo "$x / $y = $lahut<br>";
+echo "$x % $y = $jaak<br>";
+
+$mm = 340;
+$teisendacm = $mm/10;
+$teisendam = $mm/100;
+
+echo "$mm millimeetrit on $teisendacm ja meetrites on see $teisendam"."<br>";
+
+$alus = 34;
+$korgus = 12;
+$kylg = 20;
+$pindala = ($alus*$korgus)/2;
+$ymbermoot =  $kylg*3;
+
+echo "Kolmnurga pindala on $pindala ja ümbermõõt $ymbermoot";
+
